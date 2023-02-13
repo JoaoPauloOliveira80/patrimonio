8c86ddf40e39910bdbfb1bc682dacbd9fdf6ed9d
Dundaki <vigjoaopaulo@gmail.com>
2023-02-13 14:40:59 -0300

    index.html subindo

---
+++ src/test/java/com/vigjoaopaulo/investimento/InvestimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vigjoaopaulo.investimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
